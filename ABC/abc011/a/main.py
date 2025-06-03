88d994af985d8534812f3563cf898ab647819f85
RYO <ryo.na.na.ryo.na@gmail.com>
2025-06-03 12:04:17 +0900

    solve: ABC011-A, B

--- ABC/abc011/a/main.py
+++ ABC/abc011/a/main.py
@@ -5,7 +5,8 @@ def input():return sys.stdin.readline().rstrip()
 
 
 def main():
-
+    n = int(input())
+    print(n%12+1)
 
 if __name__ == '__main__':
     sys.exit(main())
--- ABC/abc011/b/main.py
+++ ABC/abc011/b/main.py
@@ -5,7 +5,8 @@ def input():return sys.stdin.readline().rstrip()
 
 
 def main():
-    
+    s = input()
+    print(s[0].upper()+s[1:].lower())
 
 
 if __name__ == '__main__':
